ec6c58b7f10f0b9d23439471874185b4bf9c30fc
mihyeon <algus8615@naver.com>
2023-08-02 09:47:05 +0900

    사진 확대 효과 추가

--- main/js/index.ts
+++ main/js/index.ts
@@ -1,49 +1,35 @@
-// const home: any = document.getElementById("btn_home"); // 0
-// const skill: any = document.getElementById('btn_skill'); // -688
-// const project: any = document.getElementById('btn_project'); // -1379
-// const headerHegiht: number = 140;
+    const navbarMenu: any = document.querySelector('.navbar__menu');
 
-// // 스크롤 이동시 좌표 확인하기
+    navbarMenu.addEventListener('click', (event: any) => {
+        const target = event.target;
+        const link = target.dataset.link;
 
-// home.addEventListener("click", () => {
-//     console.log("1")
-//     scrollTo({left:0, top:0})
-//   });
-  
-// skill.addEventListener("click", () => {
-//     scrollTo({left:0, top:-688})
-// });
-  
-// project.addEventListener("click", () => {
-//     scrollTo({left:0, top:-1379})
-// });
+        if (link == null) {
+            return;
+        } else {
+            const scroll = document.querySelector(link);
+            console.log(' scroll---- ', link.replace('#',''))
+            
+            if ( link.replace('#','') == 'home'){
+                scroll.scrollTo(0,0);
+            } else if( link.replace('#','') == 'skill'){
+                scroll.scrollTo(0, -688);
+            } else if( link.replace('#','') == 'project'){
+                scroll.scrollTo(0, -1379);
+            }
+            // if ( s)
+            scroll.scrollIntoView({behavior:"smooth"}); //option으로 애니메이션을 smooth하게 만든다
+        }
+    });
 
-
-const navbarMenu: any = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event: any) => {
-
-  const target = event.target;
-  const link = target.dataset.link;
-
-  console.log(' target ----' ,target)
-  console.log(' link ====' , link)
-  console.log(" target.dataset", target.dataset)
-  console.log('---',event.target.dataset.link);
-  
-  if (link == null) {
-    return;
-  } else {
-    const scroll = document.querySelector(link);
-    console.log(' scroll---- ', link.replace('#',''))
+    function zoomIn(event: any) {
+        event.target.style.transform = "scale(1.2)"; //1.2배 확대
+        event.target.style.zIndex = 1;
+        event.target.style.transition = "all 0.5s";// 속도
+      }
     
-    if ( link.replace('#','') == 'home'){
-        scroll.scrollTo(0,0);
-    } else if( link.replace('#','') == 'skill'){
-        scroll.scrollTo(0, -688);
-    } else if( link.replace('#','') == 'project'){
-        scroll.scrollTo(0, -1379);
-    }
-    // if ( s)
-    scroll.scrollIntoView({behavior:"smooth"}); //option으로 애니메이션을 smooth하게 만든다
-  }
-});
+      function zoomOut(event: any) {
+        event.target.style.transform = "scale(1)";
+        event.target.style.zIndex = 0;
+        event.target.style.transition = "all 0.5s";
+      }
